92fdceaf89ec4266e3cfa69ef7eda55155e42c6a
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-17 19:00:23 -0500

    Create Vite + React App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>React</h1>
+    </>
+  );
+}
+
+export default App;
