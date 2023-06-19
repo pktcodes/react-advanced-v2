f8a229a8afbf0ce1e1bbb3c6cc37c2049923dd11
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-18 23:45:50 -0500

    Error Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Starter from './tutorial/01-useState/starter/01-error-example';
+
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
+      <Starter />
     </div>
   );
 }
--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+
+  const handleClick = () => {
+    count = count + 1;
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        count
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
