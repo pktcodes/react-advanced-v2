1fd1365ffad37b52a390ba4d698ee1e1bc2c45bc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-19 23:43:58 -0500

    UseState Object Example

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('peter');
-  const [age, setAge] = useState(25);
-  const [hobby, setHobby] = useState('read books');
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'read books',
+  });
 
-  // Auto Batching- From React 18,here updating 3 states values is grouped into single update and as a result only one re-render occurs
   const displayPerson = () => {
-    setName('john');
-    setAge(30);
-    setHobby('scream at computer');
+    // setPerson({ name: 'john', age: 30, hobby: 'scream at computer' });
+    // setPerson('developer');
+    // setPerson({ job: 'developer' });
+    setPerson({ ...person, name: 'susan' });
   };
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h3>enjoys to: {hobby}</h3>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>enjoys to: {person.hobby}</h3>
       <button
         type="button"
         className="btn"
