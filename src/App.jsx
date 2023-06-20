3e46750edcf4f9dc964f8ce6e8aa107701e48ecc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-19 22:47:49 -0500

    UseState Array Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/01-useState/starter/02-useState-basics';
+import Starter from './tutorial/01-useState/starter/03-useState-array';
 
 function App() {
   return (
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(people);
+
+  const handleRemoveItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+    // setPeople(people.filter((person) => person.id !== id));
+  };
+
+  const handleClearItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        // console.log(person);
+        const { id, name } = person;
+        return (
+          <article key={id} style={{ margin: '1rem' }}>
+            <h4>{name}</h4>
+            <button
+              type="button"
+              className="btn"
+              onClick={() => {
+                handleRemoveItem(id);
+              }}
+            >
+              Remove
+            </button>
+          </article>
+        );
+      })}
+
+      <button
+        type="button"
+        className="btn"
+        style={{ background: 'red', width: '10rem', marginTop: '2rem' }}
+        onClick={handleClearItems}
+      >
+        clear Items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
