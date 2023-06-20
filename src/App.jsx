f82cf15ee4ec6c515e344d40f7920757da153bef
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-19 23:55:52 -0500

    UseState Set Function "Gotcha"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/01-useState/starter/04-useState-object';
+import Starter from './tutorial/01-useState/starter/05-useState-gotcha';
 
 function App() {
   return (
--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button type="button" className="btn" onClick={handleIncrease}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
