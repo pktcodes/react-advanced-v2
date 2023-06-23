fd93a8dd2f11c2d18701b1a4d522902315fcfefa
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-22 23:18:27 -0500

    UseEffect - Multiple Effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics';
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects';
 
 function App() {
   return (
