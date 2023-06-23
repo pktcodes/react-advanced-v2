3ae425e088d1c34cb05ffa1b0af785fe68b0f80e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-23 00:25:25 -0500

    UseEffect Fetch Challenge - Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/03-multiple-effects';
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
 
 function App() {
   return (
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,42 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchGithubUsers = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchGithubUsers();
+  }, []);
+
+  return (
+    <section>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, avatar_url, login, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
