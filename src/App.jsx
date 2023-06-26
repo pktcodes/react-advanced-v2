520b7ed891317bdffedc2db396cefd7a9ec5b618
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-26 00:02:25 -0500

    React Hooks Rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule';
 
 function App() {
   return (
