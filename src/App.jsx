de693f8c0d7482378e091e822a405c514dfcf390
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-27 21:59:37 -0500

    Toggle Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples';
+import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge';
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(false);
+
+  // const toggleAlert = () => {
+  //   if (showAlert) {
+  //     setShowAlert(false);
+  //     return;
+  //   }
+  //   setShowAlert(true);
+  // };
+
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setShowAlert(!showAlert);
+        }}
+      >
+        Toggle
+      </button>
+      {showAlert && <Alert />}
+    </div>
+  );
+};
+
+const Alert = () => {
+  return <h2 className="alert alert-danger">hello world</h2>;
 };
 
 export default ToggleChallenge;
