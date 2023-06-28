83dda5c4bf55bb1f28daaf7ab167a8064d075902
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-28 00:20:55 -0500

    UseEffect Cleanup Function - Setup/Challenge

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        toggle
+      </button>
+      {toggle && <RandomComponent />}
+    </>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('Hmm, That is Interesting.');
+  }, []);
+
+  return <h2 className="alert alert-success">hello world</h2>;
 };
 
 export default CleanupFunction;
