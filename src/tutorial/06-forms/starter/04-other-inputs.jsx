dbfc759c11b505f6715da3c382b94d57000babbe
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-01 23:53:39 -0500

    Checkbox Input

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,20 +1,32 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
+
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+
+  const handleShipping = (event) => {
+    // console.log(event.target.checked);
+    setShipping(event.target.checked);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Other Inputs</h4>
+
         {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="shipping"> Free Shipping </label>
+          <input type="checkbox" checked={shipping} onChange={handleShipping} />
         </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="framework" className="form-label">
             Framework
           </label>
         </div>
-        <button type='submit' className='btn btn-block'>
+
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
