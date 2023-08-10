f277869a710e3ada8bb83023ab9a2e7c61a813fd
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-09 22:22:17 -0500

    useRef - DOM Node

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -2,26 +2,40 @@ import { useEffect, useRef, useState } from 'react';
 
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
+  const refContainer = useRef(null);
+  // console.log('Inside Component', refContainer);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const name = refContainer.current.value;
+    console.log(name);
   };
+
+  // Runs After Initial Render
+  // useEffect(() => {
+  //   console.log('Inside useEffect', refContainer);
+  // });
 
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            ref={refContainer}
+          />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       <h1>value : {value}</h1>
-      <button onClick={() => setValue(value + 1)} className='btn'>
+      <button onClick={() => setValue(value + 1)} className="btn">
         increase
       </button>
     </div>
