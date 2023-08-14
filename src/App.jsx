32b40a59a7320063bc4f6c5794e13a8ce876b440
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-13 19:16:38 -0500

    Context API - Remove Final Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Final from './tutorial/09-context-api/final/context/Navbar';
 import Starter from './tutorial/09-context-api/starter/context/Navbar';
 
 function App() {
   return (
     <div>
-      <Final />
       <Starter />
     </div>
   );
