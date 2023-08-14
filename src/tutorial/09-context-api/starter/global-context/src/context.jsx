b373e84da63679fe2617aee43a11802f699b2a3f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-13 23:17:26 -0500

    Context API - Global Setup

---
+++ src/tutorial/09-context-api/starter/global-context/src/context.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+import { createContext, useContext, useState } from 'react';
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const AppContext = ({ children }) => {
+  const [name, setName] = useState('john');
+  return (
+    <GlobalContext.Provider value={{ name, setName }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+AppContext.propTypes = {
+  children: PropTypes.object,
+};
+
+export default AppContext;
