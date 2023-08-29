eed2764bd9024f03b8090cdb2bd0de2fe22902e6
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-28 21:44:04 -0500

    UseReducer - Challenge

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { data } from '../../../data';
+
 const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
 
@@ -7,24 +8,43 @@ const ReducerBasics = () => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
+  const clearList = () => {
+    setPeople([]);
+  };
+
+  const resetList = () => {
+    setPeople(data);
+  };
+
   return (
-    <div>
+    <div className="container">
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {people.length > 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: '2rem' }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: '2rem' }}
+          onClick={resetList}
+        >
+          reset
+        </button>
+      )}
     </div>
   );
 };
