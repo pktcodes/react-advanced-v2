206473796b38c1ab75afb2900c872170ce6630ee
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-05 01:41:53 -0500

    useTransition Hook

--- src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,43 +1,53 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
+
 const LatestReact = () => {
   const [text, setText] = useState('');
   const [items, setItems] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setText(e.target.value);
 
-    // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      // slow down CPU
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src="/vite.svg" alt="" />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
+
   return (
-    <section>
-      <form className='form'>
+    <section className="container">
+      <form className="form">
         <input
-          type='text'
-          className='form-input'
+          type="text"
+          className="form-input"
           value={text}
           onChange={handleChange}
         />
       </form>
       <h4>Items Below</h4>
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
-        }}
-      >
-        {items}
-      </div>
+      {isPending ? (
+        <div className="loading"></div>
+      ) : (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            marginTop: '2rem',
+          }}
+        >
+          {items}
+        </div>
+      )}
     </section>
   );
 };
+
 export default LatestReact;
